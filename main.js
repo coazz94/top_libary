79cb0dbf0d87b028ca831a32a91910dd44b1e428
z0047n1j <test@hotmail.com>
2022-11-18 14:44:04 +0100

    changes

--- main.js
+++ main.js
@@ -4,11 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
     addBooks()
     loadBooks();
 
-    // set the popup view to none
-    let popup = document.querySelector(".popup");
-    popup.style.display = "none";
+    // set the popup visibility to hidden
+    document.querySelector(".popup").style.visibility='hidden';
     
-
     // Fint the add new book button and load the popupscreen
     const add_book = document.querySelector("#addbutton");
 
@@ -16,33 +14,32 @@ document.addEventListener("DOMContentLoaded", () => {
         enablePopup(true);
     })
 
-
+    // add a new book to the libary
     const button_add = document.querySelector("#book_to_add");
 
-    button_add.addEventListener("click", (event) => {
-        addBookToLibary();
-        enablePopup(false);
-        document.querySelector(".books").innerHTML = "";
-        loadBooks();
+    button_add.addEventListener("click", () => {
+
+        if (addBookToLibary()){
+            enablePopup(false);
+            loadBooks();
+        }
     })
 
 })
 
+let myLibary = [];
 
 
 function enablePopup(state){
 
-    document.querySelector("#addbutton").style.display = state ? "none" : "block";
-    document.querySelector(".books").style.display = state ? "none" : "block";
-    document.querySelector(".popup").style.display = state ? "block" : "none";
+
+    document.querySelector("#addbutton").style.visibility = state ? "hidden" : "visible";
+    document.querySelector(".books").style.visibility = state ? "hidden" : "visible";
+    document.querySelector(".popup").style.visibility = state ? "visible" : "hidden";
 
 }
 
-
-let myLibary = [];
-
-
-function Book(title, author, read, pages){
+function Book(title, author, read, pages=0, id){
     this.title = title
     this.author = author
     this.read = read
@@ -59,8 +56,10 @@ function addBooks(){
     myLibary.push(book1, book2, book3)
 }
 
-
 function loadBooks(){
+
+    // remove the displayed books
+    document.querySelector(".books").innerHTML = "";
 
     // get the place where to books are gonna be stored
     const books_div = document.querySelector(".books");
@@ -68,45 +67,126 @@ function loadBooks(){
     // loop over the Array with the books and add add for each book a new div
     for(let i = 0; i < myLibary.length; i++){
 
+        // make the variables needed
         let book = myLibary[i]
+        let read = book.read ? "read": "not read"
+        let pages = parseInt(book.pages) > 0 ? book.pages : "/";
+        let id = myLibary.indexOf(myLibary[i])
 
         // new book, id= book + counter
-        let book_el = document.createElement("div")
-        book_el.setAttribute("id", `book_${i+1}`)
+        let book_el = document.createElement("div");
+        book_el.className=`card book_${i+1}`;
 
+        let test = document.createElement("div");
+        test.className=`card-body`;
+
+        // make a card element fot each book
         book_el.innerHTML = `
-        <p>${book.title}</p>
-        <p>${book.author}</p>
-        <p>${book.pages}</p>
-        <p>${book.status}</p>
+        <div class="card-body">
+          <h5 class="card-title">${book.title}</h5>
+          <p class="card-text">Author: ${book.author} </p>
+          <p class="card-text">Pages: ${pages} </p>
+          <p class="card-text"> ${read}</p>
+        </div>
         `
+        // make the buttons
+        let buttons = makeButtonsForCard(id);
 
+        // append them to the card
+        book_el.append(buttons)
 
+        // append the card to the books div
         books_div.append(book_el)
         
     }
 }
 
+function makeButtonsForCard(id){
+
+        // make  div for the button
+        let buttons= document.createElement("div");
+        buttons.className = "button_position";
+
+        // make the button
+        let remove_b = document.createElement("button");
+        remove_b.setAttribute("id", `remove_b`);
+        remove_b.innerHTML = "remove this book";
+
+        // add a Eventlistenr to the button
+        remove_b.addEventListener("click", () => {
+            // delete the element at the position id, load the books again
+            myLibary.splice(id, 1)
+            loadBooks()
+        })
+
+        let read_b = document.createElement("button");
+        read_b.setAttribute("id", `read_b`);
+        read_b.innerHTML = "mark as read";
+
+        // add a Eventlistenr to the button
+        read_b.addEventListener("click", () => {
+            // Change the status of the object by its lasts state
+            myLibary[id].read ?  myLibary[id].read = false : myLibary[id].read = true
+            loadBooks()
+        })
+
+        // append the 2 buttons to the buttons div
+        buttons.append(remove_b, read_b)
+
+        // return the div
+        return buttons
+}
 
 function addBookToLibary(){
 
+    // Grab the inputed values
+    const title = document.querySelector("#title");
+    const author = document.querySelector("#author");
+    const pages = document.querySelector("#pages");
+    const read = document.querySelector("#read");
 
+    // Check if the title author
+    let state = checkInput(title.value, author.value);
+
+    // if so, proceed and make a new book and add it to the libary
+    if (state){
+        let book = new Book(title.value, author.value, read.checked, pages.value);
+        myLibary.push(book);
+        // clean the input fields
+        cleanPopup(title, author, read, pages);
+        // Return True
+        return true;
+    }
+    // if ome error return false
+    return false
+}
+
+function checkInput(title, author){
     
-    const title = document.querySelector("#title").value;
-    const author = document.querySelector("#author").value;
-    const pages = document.querySelector("#pages").value;
-    const read = document.querySelector("#read").value;
-
-    let book = new Book(title, author, false, pages);
-
-    myLibary.push(book)
-
+    // title or author arent allowed to be empty
+    if (title.length <= 4 || author === ""){
+        document.querySelector("#errormessage")
+        .innerHTML = "You have to input a Title, Author and check the Read button"
+        return false
+    }else{
+        return true
+    }
 
 }
 
+function cleanPopup(title, author, read){
 
+    // Clear the error message and the input field
+    document.querySelector("#errormessage").innerHTML = "";
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    read.checked = false;
+
+}
 
 function mySubmitFunction(e) {
     e.preventDefault();
     return false;
-  }
+}
+
